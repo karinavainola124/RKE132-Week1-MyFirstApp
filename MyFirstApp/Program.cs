adf40c88794467351e8c520eec7b4a8080cd2841
karinavainola124 <karina.vainola@gmail.com>
2024-11-21 23:07:59 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName;
+
+string userName = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //Output
+
